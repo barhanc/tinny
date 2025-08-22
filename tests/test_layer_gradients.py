9bf3c9965a462f6f11a322bdee61c17e59799cd1
Bartosz Hanc <bartosz.hanc02@gmail.com>
2025-08-22 18:09:51 +0200

    Add parameters gradient tests

--- tests/test_layer_gradients.py
+++ tests/test_layer_gradients.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from copy import deepcopy
 from typing import Literal
 from itertools import pairwise
 
@@ -9,7 +10,7 @@ import pytest
 import numpy as np
 import hypothesis.extra.numpy
 
-from numpy.typing import NDArray
+from numpy.typing import NDArray, DTypeLike
 from hypothesis import given, assume
 from hypothesis import strategies as some
 
@@ -46,11 +47,56 @@ def compute_grad_x_backprop(
     return grad_x[i]
 
 
-def some_shape(ndim: int, min_length: int, max_length):
+def compute_grad_params_backprop(
+    f: nn.Layer,
+    x: NDArray,
+    idxs: list[tuple[int, ...]],
+    j: tuple[int, ...],
+) -> list[float]:
+    y = f.forward(x, training=True)
+    grad_y = np.zeros_like(y)
+    grad_y[j] = 1.0
+    f.backward(grad_y)
+
+    grads = []
+    for i, grad_p in zip(idxs, f.gradients()):
+        assert grad_p is not None
+        grads.append(grad_p[i])
+
+    return grads
+
+
+def compute_grad_params_finite_diff(
+    f: nn.Layer,
+    x: NDArray,
+    idxs: list[tuple[int, ...]],
+    j: tuple[int, ...],
+    eps: float = 1e-8,
+) -> list[float]:
+    grads = []
+
+    for k, i in enumerate(idxs):
+        f1 = deepcopy(f)
+        f2 = deepcopy(f)
+
+        f1.parameters()[k][i] += eps
+        f2.parameters()[k][i] -= eps
+
+        dy = f1.forward(x, training=True) - f2.forward(x, training=True)
+        grads.append(dy[j] / (2 * eps))
+
+    return grads
+
+
+def some_shape(ndim: int, min_length: int, max_length: int):
     return some.tuples(*(some.integers(min_length, max_length) for _ in range(ndim)))
 
 
-def some_tensor(*shape: int, dtype=np.float64, domain: tuple[float, float] = (-5.0, +5.0)):
+def some_tensor(
+    *shape: int,
+    dtype: DTypeLike = np.float64,
+    domain: tuple[float, float] = (-5.0, +5.0),
+):
     return hypothesis.extra.numpy.arrays(dtype, shape, elements=some.floats(*domain))
 
 
@@ -70,19 +116,19 @@ def test_activation_grad_x_correctness(
     shape: tuple[int, int],
     data: some.DataObject,
 ):
-    i = data.draw(some_multi_index(*shape), label="Multi-index")
-    x = data.draw(some_tensor(*shape), label="Input tensor")
     f = activation_type()
+    i = data.draw(some_multi_index(*shape), "Multi-index")
+    x = data.draw(some_tensor(*shape), "Input tensor")
 
     # Skip gradient check at ReLU non-differentiable point
     if activation_type is nn.ReLU:
         assume(not np.isclose(x[i], 0.0))
 
-    grad_bp = compute_grad_x_backprop(f, x, i, i)
-    grad_fd = compute_grad_x_finite_diff(f, x, i, i)
+    grad_bp = compute_grad_x_backprop(deepcopy(f), x, i, i)
+    grad_fd = compute_grad_x_finite_diff(deepcopy(f), x, i, i)
 
     logging.info(
-        "%s ∂Yj/∂Xi , %+12.6f , %+12.6f , %.6f",
+        "%s ∂Yj/∂Xi , %+8.6f , %+8.6f , %.6f",
         activation_type.__name__,
         grad_bp,
         grad_fd,
@@ -106,16 +152,16 @@ def test_linear_grad_x_correctness(
     init_method: Literal["He", "Xavier"],
     data: some.DataObject,
 ):
-    i = data.draw(some_multi_index(batch_size, in_features), label="Input multi-index")
-    j = data.draw(some_multi_index(batch_size, out_features), label="Output multi-index")
-    x = data.draw(some_tensor(batch_size, in_features), label="Input tensor")
-    f = nn.Linear(in_features, out_features, init_method)
+    f = nn.Linear(in_features, out_features, init_method, dtype=np.float64)
+    i = data.draw(some_multi_index(batch_size, in_features), "Input multi-index")
+    j = data.draw(some_multi_index(batch_size, out_features), "Output multi-index")
+    x = data.draw(some_tensor(batch_size, in_features), "Input tensor")
 
-    grad_bp = compute_grad_x_backprop(f, x, i, j)
-    grad_fd = compute_grad_x_finite_diff(f, x, i, j)
+    grad_bp = compute_grad_x_backprop(deepcopy(f), x, i, j)
+    grad_fd = compute_grad_x_finite_diff(deepcopy(f), x, i, j)
 
     logging.info(
-        "Linear ∂Yj/∂Xi , %+12.6f , %+12.6f , %.6f",
+        "Linear ∂Yj/∂Xi , %+8.6f , %+8.6f , %.6f",
         grad_bp,
         grad_fd,
         abs(grad_bp - grad_fd),
@@ -124,8 +170,43 @@ def test_linear_grad_x_correctness(
     assert np.isclose(grad_bp, grad_fd)
 
 
-def test_linear_grad_params_correctness():
-    assert False
+@given(
+    some.integers(1, 64),
+    some.integers(1, 64),
+    some.integers(1, 64),
+    some.one_of(some.just("He"), some.just("Xavier")),
+    some.data(),
+)
+def test_linear_grad_params_correctness(
+    batch_size: int,
+    in_features: int,
+    out_features: int,
+    init_method: Literal["He", "Xavier"],
+    data: some.DataObject,
+):
+    f = nn.Linear(in_features, out_features, init_method, dtype=np.float64)
+    idxs = [
+        data.draw(some_multi_index(in_features, out_features), "Weight multi-index"),
+        data.draw(some_multi_index(out_features), "Bias multi-index"),
+    ]
+    j = data.draw(some_multi_index(batch_size, out_features), "Output multi-index")
+    x = data.draw(some_tensor(batch_size, in_features), "Input tensor")
+
+    # Skip gradient check if input is all 0s, as in such case it is not possible to compute grad_w
+    # using finite difference method
+    assume(not np.allclose(x[:, idxs[0][0]], 0.0))
+
+    grads_bp = compute_grad_params_backprop(deepcopy(f), x, idxs, j)
+    grads_fd = compute_grad_params_finite_diff(deepcopy(f), x, idxs, j)
+
+    logging.info(
+        "Linear ∂Yj/∂θi , w:  %+8.6f ,  %+8.6f , b:  %+8.6f ,  %+8.6f",
+        grads_bp[0],
+        grads_fd[0],
+        grads_bp[1],
+        grads_fd[1],
+    )
+    assert np.allclose(grads_bp, grads_fd)
 
 
 @given(
@@ -154,16 +235,24 @@ def test_conv2d_grad_x_correctness(
 
     assume(H_out >= 1 and W_out >= 1)
 
+    f = nn.Conv2D(
+        in_channels,
+        out_channels,
+        kernel_size,
+        strides,
+        padding,
+        init_method,
+        dtype=np.float64,
+    )
     i = data.draw(some_multi_index(*in_shape), "Input multi-index")
     j = data.draw(some_multi_index(B, out_channels, H_out, W_out), "Output multi-index")
     x = data.draw(some_tensor(*in_shape), "Input image")
-    f = nn.Conv2D(in_channels, out_channels, kernel_size, strides, padding, init_method)
 
-    grad_bp = compute_grad_x_backprop(f, x, i, j)
-    grad_fd = compute_grad_x_finite_diff(f, x, i, j)
+    grad_bp = compute_grad_x_backprop(deepcopy(f), x, i, j)
+    grad_fd = compute_grad_x_finite_diff(deepcopy(f), x, i, j)
 
     logging.info(
-        "Conv2D ∂Yj/∂Xi , %+12.6f , %+12.6f , %.6f",
+        "Conv2D ∂Yj/∂Xi , %+8.6f , %+8.6f , %.6f",
         grad_bp,
         grad_fd,
         abs(grad_bp - grad_fd),
@@ -172,8 +261,63 @@ def test_conv2d_grad_x_correctness(
     assert np.isclose(grad_bp, grad_fd)
 
 
-def test_conv2d_grad_params_correctness():
-    assert False
+@given(
+    some_shape(ndim=4, min_length=1, max_length=8),
+    some.integers(1, 16),
+    some.tuples(some.integers(1, 5), some.integers(1, 5)),
+    some.tuples(some.integers(1, 2), some.integers(1, 2)),
+    some.tuples(some.integers(0, 2), some.integers(0, 2)),
+    some.one_of(some.just("He"), some.just("Xavier")),
+    some.data(),
+)
+def test_conv2d_grad_params_correctness(
+    in_shape: tuple[int, int, int, int],
+    out_channels: int,
+    kernel_size: tuple[int, int],
+    strides: tuple[int, int],
+    padding: tuple[int, int],
+    init_method: Literal["Xavier", "He"],
+    data: some.DataObject,
+):
+    # Standard notation when describing convolutions, so pylint: disable=invalid-name
+    B, in_channels, H, W = in_shape
+    H_out = int(1 + (H + 2 * padding[0] - kernel_size[0]) / strides[0])
+    W_out = int(1 + (W + 2 * padding[1] - kernel_size[1]) / strides[1])
+    # pylint: enable=invalid-name
+
+    assume(H_out >= 1 and W_out >= 1)
+
+    f = nn.Conv2D(
+        in_channels,
+        out_channels,
+        kernel_size,
+        strides,
+        padding,
+        init_method,
+        dtype=np.float64,
+    )
+    idxs = [
+        data.draw(
+            some_multi_index(out_channels, in_channels * kernel_size[0] * kernel_size[1]),
+            "Kernel multi-index",
+        ),
+        data.draw(some_multi_index(out_channels, 1), "Bias multi-index"),
+    ]
+    j = data.draw(some_multi_index(B, out_channels, H_out, W_out), "Output multi-index")
+    x = data.draw(some_tensor(*in_shape), "Input image")
+
+    grads_bp = compute_grad_params_backprop(deepcopy(f), x, idxs, j)
+    grads_fd = compute_grad_params_finite_diff(deepcopy(f), x, idxs, j)
+
+    logging.info(
+        "Linear ∂Yj/∂θi , w:  %+8.6f ,  %+8.6f , b:  %+8.6f ,  %+8.6f",
+        grads_bp[0],
+        grads_fd[0],
+        grads_bp[1],
+        grads_fd[1],
+    )
+
+    assert np.allclose(grads_bp, grads_fd)
 
 
 @given(
@@ -192,15 +336,15 @@ def test_sequential_grad_flow_correctness(
         layers.append(nn.Sigmoid())
 
     f = nn.Sequential(*layers)
-    i = data.draw(some_multi_index(batch_size, dims[0]), label="Input multi-index")
-    j = data.draw(some_multi_index(batch_size, dims[-1]), label="Output multi-index")
-    x = data.draw(some_tensor(batch_size, dims[0]), label="Input tensor")
+    i = data.draw(some_multi_index(batch_size, dims[0]), "Input multi-index")
+    j = data.draw(some_multi_index(batch_size, dims[-1]), "Output multi-index")
+    x = data.draw(some_tensor(batch_size, dims[0]), "Input tensor")
 
-    grad_bp = compute_grad_x_backprop(f, x, i, j)
-    grad_fd = compute_grad_x_finite_diff(f, x, i, j)
+    grad_bp = compute_grad_x_backprop(deepcopy(f), x, i, j)
+    grad_fd = compute_grad_x_finite_diff(deepcopy(f), x, i, j)
 
     logging.info(
-        "Sequential ∂Yj/∂Xi , %+12.6f , %+12.6f , %.6f",
+        "Sequential ∂Yj/∂Xi , %+8.6f , %+8.6f , %.6f",
         grad_bp,
         grad_fd,
         abs(grad_bp - grad_fd),
